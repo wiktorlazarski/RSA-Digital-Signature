2e6efc3f84fdde2b37d7410573bc9058e8327f3e
Wiktor Łazarski <wjlazarski@gmail.com>
2019-12-05 22:04:04 +0100

    RSA key generator that do not use any optimization to produce keys

--- rsa_keys.py
+++ rsa_keys.py
@@ -1,30 +1,51 @@
 from random import randint
-
+from math import gcd
 
 class RSAKeys:
-	'''Class produce and represents keys for RSA algorithm.'''
+	'''Class produces and represents keys for RSA algorithm.'''
+
+	@staticmethod
+	def list_primes(lo, hi):	
+		primes = []
+		for num in range(lo, hi, 2):
+			for i in range(2, num):
+				if num % i == 0:
+					break
+			else:
+				primes.append(num)
+
+		return primes
+
+	@staticmethod
+	def generate_prime_pair(lo, hi):
+		primes = RSAKeys.list_primes(lo, hi)
+
+		f_idx = randint(0, len(primes) - 1)
+		first = primes.pop(f_idx)
+		
+		s_idx = randint(0, len(primes) - 1)
+		second = primes[s_idx]
+
+		return (first, second)
 
 	def __init__(self, bits=1024):
 		self.e, self.d = self.generate(bits)
 
 	def generate(self, bits):
-		primes = RSAKeys.list_primes(2 ** bits - 1, 2 ** (bits - 1))
+		p, q = RSAKeys.generate_prime_pair(2 ** (bits - 1) + 1, 2 ** bits)
 		
+		self.n = p * q
+		phi_n = (p - 1) * (q - 1)
 
-		p_idx = randint(0, len(primes))
-		p = primes[p_idx]
-		del primes
-		
-		q_idx = randint(0, len(primes))
-		q = primes[q_idx]
-		
+		return self.generate_keys(phi_n)
 
-		return (1, 2)
+	def generate_keys(self, phi_n):
+		primes = RSAKeys.list_primes(1, phi_n)
+		primes = [p for p in primes if gcd(p, phi_n) == 1]
 
-	@staticmethod
-	def list_primes(lo, hi):
-		return [1, 2, 3, 5, 7]
+		e = primes[randint(0, len(primes) - 1)]
+	
+		d = 1
+		while (e * d) % phi_n != 1: d = d + 1
 
-
-keys = RSAKeys()
-print("p = {}\nq = {}".format(keys.e, keys.d))
+		return e, d
